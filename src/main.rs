96435709ac23a15c4fc827ab30815f04309cf5aa
Juan Patino <juan9patino9@gmail.com>
2023-10-19 15:06:30 -0400

    main.rs updated to allow 'index' and 'files' get requests.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 #[macro_use] extern crate rocket;
 
 use rocket::fs::NamedFile;
-use std::path::{Path};
+use std::path::{Path, PathBuf};
 
+// Route for specific file paths
+#[get("/<file..>", rank = 2)]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
+}
+
+// Default route when no specific path is provided, serves index.html
 #[get("/")]
-async fn home() -> Option<NamedFile>{
+async fn index() -> Option<NamedFile> {
     NamedFile::open(Path::new("static/index.html")).await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![home])
+    rocket::build().mount("/", routes![index, files])
 }
