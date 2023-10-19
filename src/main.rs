e4df8d532285bbc119dda0307c8a7d044fb2f4e0
Juan Patino <juan9patino9@gmail.com>
2023-10-19 11:43:43 -0400

    get method in controller (main.rs) works

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 #[macro_use] extern crate rocket;
 
 use rocket::fs::NamedFile;
-use rocket::Route;
 use std::path::{Path, PathBuf};
 
-// #[get("/")]
-// fn hello() -> &'static str {
-//     "Hello, world!"
-// }
-
 #[get("/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile>{
-    NamedFile::open(Path::new("static/").join(file)).ok()
+async fn files(file: PathBuf) -> Option<NamedFile>{
+    NamedFile::open(Path::new("static/").join(file)).await.ok()
 }
 
 #[launch]
